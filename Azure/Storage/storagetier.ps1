bd041d7ab558d2918bf56a23fa3ea5e944965cee
Eliaquim Brandao <49751389+eliaquimbrandao@users.noreply.github.com>
2021-08-30 11:15:48 +0100

    Adding Storage Blob Tier Script

--- Azure/Storage/storagetier.ps1
+++ Azure/Storage/storagetier.ps1
@@ -1,32 +1,32 @@
 # Connect to Az  Account
 Connect-AzAccount
 
-# Declare variable
+# Declare SubscriptionID variable
 $subID = "MySubID"
 
 # Choose subscription
 Select-AzSubscription -SubscriptionId "$subID"
 
-# Declare variable
+# Declare variable Storage and Resource Group
 $strAccountName = "MyStorageAccounName"
 $strAccountRG = "MyRG"
 
-# create context
+# Create Azure Storage Context
 $stCtx = New-AzStorageContext -StorageAccountName $strAccountName -StorageAccountKey ((Get-AzStorageAccountKey -ResourceGroupName $strAccountRG -Name $strAccountName).Value[0])
 
-# fetch containers
+# Fetch Containers
 $containers = Get-AzStorageContainer -Context $stCtx
 
-# placeholder to hold file list
+# Hold File List
 $array = @();
 
-# zero out our total
+# Zero out our total
 $length = 0
 
-# this loops through the list of blobs and retrieves the length for each blob and adds it to the total
+# Loops through the list of blobs and retrieves the length for each blob and adds it to the total
 $listOfBlobs | ForEach-Object {$length = $length + $_.Length}
 
-# outer loop
+# Loop
 foreach($container in $containers)
 {
     # fetch blobs in current container
@@ -36,7 +36,7 @@ foreach($container in $containers)
     
 $totalarray = @($array).Count  
 
-#get total tier/size/percentage
+# Get total tier/size/percentage
 $array | Group-Object AccessTier | %{
      New-Object psobject -Property @{
         AccessTier = $_.Name
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # powershell-scripts
+
+## Technologies
+
+The following tools were used in this project:
+
+- [Azure PowerShell](https://docs.microsoft.com/en-us/powershell/azure/?view=azps-6.3.0)
+
+## Additional information
+
+- Codes continued to be improved
+- I'm not an expert but I try my best :smile:
+- If you want to contribute, you are always welcome.
+
+## License
+
+Made with :heart: by <a href="https://github.com/eliaquimbrandao" target="_blank">Eliaquim Brandao</a>
+
+[MIT](https://choosealicense.com/licenses/mit/)
